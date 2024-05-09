db74a9fbf8f665ba1a450fbfd1d041478d0a35cf
Henry Lee <henryleestudio@gmail.com>
2024-05-09 10:29:00 -0400

    price and product title updating when clicked click onSwitch

--- assets/theme.js
+++ assets/theme.js
@@ -4285,7 +4285,18 @@ onSwatchChanged_fn = async function (event, target) {
     url.searchParams.set("variant", firstMatchingVariant["id"]);
     link.href = `${url.pathname}${url.search}${url.hash}`;
   });
-  if (!firstMatchingVariant.hasOwnProperty("featured_media")) {
+
+  // For collection page and featured product section  updating title and price 5.9.2024 
+  this.querySelector('.product-card__info .product-title').innerText = this.product["title"] + " - " + firstMatchingVariant["title"];
+ var price_html = ``;
+ if(firstMatchingVariant.price < firstMatchingVariant.compare_at_price){
+ price_html += `<sale-price class="h6 text-on-sale">${formatMoney(firstMatchingVariant["price"], currencyFormat)}</sale-price><compare-at-price class="h6 text-subdued line-through">${formatMoney(firstMatchingVariant["compare_at_price"], currencyFormat)}</compare-at-price>`;
+ }else{
+ price_html += `<sale-price class="h6 text-subdued">${formatMoney(firstMatchingVariant["price"], currencyFormat)}</sale-price>`;
+ }
+ this.querySelector('.product-card__info price-list').innerHTML = price_html;
+// end of the code 
+ if (!firstMatchingVariant.hasOwnProperty("featured_media")) {
     return;
   }
   const primaryMediaElement = this.querySelector(
